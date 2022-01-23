d8568f9212660e82f1984b0ca3b66a5537395b31
Zhorge <Andrei.zhornick@yandex.ru>
2022-01-23 21:40:42 +0300

    Initial commit

---
+++ src/test/java/org/WebUiTests/App2Test.java
@@ -0,0 +1,25 @@
+package org.WebUiTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class App2Test {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("App");
+        assertTrue(true);
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1() {
+        System.out.println("App1");
+        assertTrue(1 == 1);
+    }
+}
---
+++ src/test/java/org/WebUiTests/AppTest.java
@@ -0,0 +1,25 @@
+package org.WebUiTests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("AppTest");
+        assertTrue(true);
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1() {
+        System.out.println("AppTest");
+        assertTrue(1 == 2);
+    }
+}
